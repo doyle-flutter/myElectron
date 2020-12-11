9fd3e441ac0d6eac1935d160224d9a4767df8397
doyle <il4479@hanmail.net>
2020-12-11 19:36:17 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  mainWindow.loadFile(path.join(__dirname, '/src/pages/mainPage.html'));
+  mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
